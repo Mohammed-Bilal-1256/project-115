1b41e07c92b2a50d0890645277933da21aab7fc7
Mohammed-Bilal-1256 <87923015+Mohammed-Bilal-1256@users.noreply.github.com>
2022-04-13 13:55:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,39 @@
-function preload(){
+var nosex = 0 ;
+var nosey = 0 ;
+function preload() {
 }
 
-function setup(){
-    canvas = createCanvas(300,300);
+function setup() {
+    canvas = createCanvas(300, 300);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(300,300);
+    video.size(300, 300);
     video.hide();
 
-    poseNet = ml5.poseNet(video,modelLoaded);
-    poseNet.on('pose',gotPose);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+ function modelLoaded(){
+    console.log("Posenet Is Initialised");
 }
 
-function draw(){
-    image(video,0,0,300,300);
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log("results");
+        nosex = results[0].pose.nose.x;
+        nosey = results[0].pose.nose.y;
+        console.log("nose x =" + nosex);
+        console.log("nose y =" + nosey);
+    }
 }
 
-function take_snapshot(){
-    save("my_picture.png");
+function draw() {
+    image(video, 0, 0, 300, 300);
+    fill("red");
+    stroke("red");
+    circle(nosex, nosey ,20);
 }
 
-function modelLoaded(){
-    console.log("poseNet Is Initialized");
-}
-
-function gotPose(results){
-    if(results.length > 0){
-        console.log(results);
-        console.log("nose x: "+results[0].pose.nose.x);
-        console.log("nose y: "+results[0].pose.nose.y);
-    };
+function take_snapshot() {
+    save('my_filter_image.png');
 }
